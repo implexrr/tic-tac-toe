4cb83195a07a10d049b6baaf8be3ef10e3c646a6
Chenhao Wang <implexrr@gmail.com>
2022-11-28 15:05:56 -0500

    Begin on checkRow function

--- js-files/index.js
+++ js-files/index.js
@@ -2,6 +2,8 @@
 const gameBoard = (() => {
   // Set array size
   const arrSize = 3;
+  const gameArrayStart = 0;
+  const gameArrayEnd = 2;
 
   // Create initial game array
   // First array row
@@ -17,7 +19,7 @@ const gameBoard = (() => {
   }
 
   // Make the following functions/objects public access
-  return {gameArray};
+  return {gameArray, gameArrayStart, gameArrayEnd};
 })();
 
 
@@ -25,9 +27,30 @@ const gameBoard = (() => {
 
 // Method for defining gameController object
 const gameController = (() => {
-  const mark = (symbol, x, y) => {gameBoard.gameArray[x][y] = symbol};
+  const mark = (symbol, x, y) => {
+    gameBoard.gameArray[x][y] = symbol;
+    console.log(checkRow(symbol, x, y));
+  };
   const hello = () => console.log('hello');
-  return {mark, hello};
+  const checkRow = (symbol, x, y) => {
+    if (y == gameBoard.gameArrayStart) {
+      if ((gameBoard.gameArray[x][y] == gameBoard.gameArray[x][y + 1]) && (gameBoard.gameArray[x][y] == gameBoard.gameArray[x][y + 2])) {
+        return true;
+      }
+    }
+    else if (y == gameBoard.gameArrayEnd) {
+      if ((gameBoard.gameArray[x][y] == gameBoard.gameArray[x][y - 1]) && (gameBoard.gameArray[x][y] == gameBoard.gameArray[x][y - 2])) {
+        return true;
+      }
+    }
+    else {
+      if ((gameBoard.gameArray[x][y] == gameBoard.gameArray[x][y - 1]) && (gameBoard.gameArray[x][y] == gameBoard.gameArray[x][y + 1])) {
+        return true;
+      }
+    }
+    return false;
+  };
+  return {mark, hello, checkRow};
 })();
 
 
@@ -51,4 +74,8 @@ function checkRows () {
 
 function checkColumns () {
   
+}
+
+function checkWin () {
+  (checkDiagonal || checkRows || checkColumns) ? true : false;
 }
