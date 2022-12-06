a8e22a7aad278e722a46c9a2862d44521f56ef4f
Chenhao Wang <implexrr@gmail.com>
2022-12-05 21:45:42 -0500

    unsure

--- js-files/index.js
+++ js-files/index.js
@@ -20,10 +20,6 @@ const gameBoard = (() => {
   // Create variable for blank victory message
   const victoryMessage = document.querySelector("#victory-message");
 
-  // Add functionality to the play again button
-  const playAgainButton = document.querySelector("#play-again");
-  playAgainButton.addEventListener("click", playAgain);
-
   // Create initial game array
   const gameArray = new Array(arrSize);
   for (let i = 0; i < arrSize; i++) {
@@ -34,7 +30,21 @@ const gameBoard = (() => {
       gameArray[i][j] = "";
     }
   }
+  
+  // Proxy function for gameController.mark
+  const markWithPlayerSymbol = (e, player) => {
+    gameController.mark(gameBoard[player].symbol, parseInt(e.target.dataset.yCoord), parseInt(e.target.dataset.xCoord));
+    gameBoard.player1Turn = !gameBoard.player1Turn;
+    e.target.textContent = gameBoard[player].symbol;
+  }
 
+  // Adds mark based on whose turn it is
+  const addMark = (e) => {
+    if (e.target.textContent == "") {
+      (gameBoard.player1Turn) ? markWithPlayerSymbol(e, "player1") : markWithPlayerSymbol(e, "player2");
+    }
+   }
+  
   // Generate board from array;
   const generate = () => {
     for (let i = 0; i < arrSize; i++) {
@@ -48,15 +58,8 @@ const gameBoard = (() => {
       }
     }
   }
-  
-  // Adds mark based on whose turn it is
-  const addMark = (e) => {
-    if (e.target.textContent == "") {
-      (gameBoard.player1Turn) ? markWithPlayerSymbol(e, "player1") : markWithPlayerSymbol(e, "player2");
-    }
-   }
 
-  // Start new game
+
   const playAgain = () => {
     displayController.resetFormDisplay("none", "flex");
     displayController.resetBoxes();
@@ -66,14 +69,6 @@ const gameBoard = (() => {
     gameBoard.player1Turn = true;
   }
 
-  // Proxy function for gameController.mark
-  const markWithPlayerSymbol = (e, player) => {
-    gameController.mark(gameBoard[player].symbol, parseInt(e.target.dataset.yCoord), parseInt(e.target.dataset.xCoord));
-    gameBoard.player1Turn = !gameBoard.player1Turn;
-    e.target.textContent = gameBoard[player].symbol;
-  }
-
-  // Reset data associated with game array
   const resetGameArray = () => {
     for (let i = 0; i < arrSize; i++) {
       for (let j = 0; j < arrSize; j++) {
@@ -81,6 +76,9 @@ const gameBoard = (() => {
       }
     }
   }
+
+  const playAgainButton = document.querySelector("#play-again");
+  playAgainButton.addEventListener("click", playAgain);
 
   // Make the following functions/objects public access
   return {player1, player2, player1Turn, numberOfMarks, maxMarks, victoryMessage, gameArray, gameArrayStart, gameArrayEnd, boardDisplay, addMark, generate};
